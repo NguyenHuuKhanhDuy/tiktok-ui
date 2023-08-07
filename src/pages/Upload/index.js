a45939fd358ad3b2fe7306583f55d212b470ab1c
TITANCORP\duy.nguyenh <duy.nguyenh@titancorpvn.com>
2023-08-07 15:49:52 +0700

    Cấu hình Router/Layout cho dự án

---
+++ src/pages/Upload/index.js
@@ -0,0 +1,10 @@
+import Header from '~/components/Layouts/components/Header';
+function Upload() {
+    return (
+        <div>
+            <div className="content">Upload</div>
+        </div>
+    );
+}
+
+export default Upload;
